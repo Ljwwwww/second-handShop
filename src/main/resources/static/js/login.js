e3e4770d6c8da48f0928b0b7f8fdf2428d900da6
lbw <1519131840@qq.com>
2020-07-08 15:29:38 +0800

    第三次更改

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -5,12 +5,6 @@ const app = new Vue({
         phone: ''
     },
     methods: {
-        judgePhone() {
-            if (!(/^1(3|4|5|6|7|8|9)\d{9}$/.test(this.phone))){
-                layer.msg("手机号码有误，请重填")
-                return false
-            }
-            layer.msg("验证码已发送")
-        }
+
     }
 })
